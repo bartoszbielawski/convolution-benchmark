74d2a4508af8fd7ca02168f33f31adc91e1a4e10
Bartosz Bielawski <bartosz.bielawski@gmail.com>
2015-01-23 11:37:04 +0100

    Moved implementations to a separate file

--- main.cpp
+++ main.cpp
@@ -10,222 +10,6 @@
 using namespace std;
 
 
-longSample conv_split_unrolled4_sep_result(sample* data, sample* coeffs, int len, int last_idx)
-{      
-    sample *dp = data;
-    sample *cp = coeffs;
-    longSample a[4] = {0.0, 0.0, 0.0, 0.0};
-   
-    dp = data+last_idx;
-    int cnt = len - last_idx;
-    for (; cnt >= 4; cnt -= 4)
-    {
-        a[0] += (longSample)*dp++ * (longSample)*cp++;
-        a[1] += (longSample)*dp++ * (longSample)*cp++;
-        a[2] += (longSample)*dp++ * (longSample)*cp++;
-        a[3] += (longSample)*dp++ * (longSample)*cp++;
-    }   
-
-    while (cnt--)
-    {
-        a[0] += (longSample)*dp++ * (longSample)*cp++; 
-    }
-
-    
-    dp = data;
-    cnt = last_idx;
-    for (; cnt >= 4; cnt -= 4)
-    {
-        a[0] += (longSample)*dp++ * (longSample)*cp++;
-        a[1] += (longSample)*dp++ * (longSample)*cp++;
-        a[2] += (longSample)*dp++ * (longSample)*cp++;
-        a[3] += (longSample)*dp++ * (longSample)*cp++;
-    }
-
-    while (cnt--)
-    {
-        a[1] += (longSample)*dp++ * (longSample)*cp++; 
-    }
-
-    return a[0] + a[1] + a[2] + a[3];
-}
-
-longSample conv_split_unrolled4_sep_result_no_long(sample* data, sample* coeffs, int len, int last_idx)
-{      
-    sample *dp = data;
-    sample *cp = coeffs;
-    longSample a[4] = {0.0, 0.0, 0.0, 0.0};
-   
-    dp = data+last_idx;
-    int cnt = len - last_idx;
-    for (; cnt >= 4; cnt -= 4)
-    {
-        a[0] += *dp++ * *cp++;
-        a[1] += *dp++ * *cp++;
-        a[2] += *dp++ * *cp++;
-        a[3] += *dp++ * *cp++;
-    }   
-
-    while (cnt--)
-    {
-        a[0] += *dp++ * *cp++; 
-    }
-
-    
-    dp = data;
-    cnt = last_idx;
-    for (; cnt >= 4; cnt -= 4)
-    {
-        a[0] += *dp++ * *cp++;
-        a[1] += *dp++ * *cp++;
-        a[2] += *dp++ * *cp++;
-        a[3] += *dp++ * *cp++;
-    }
-
-    while (cnt--)
-    {
-        a[1] += *dp++ * *cp++; 
-    }
-
-    return a[0] + a[1] + a[2] + a[3];
-}
-
-longSample conv_split_unrolled2_sep_result(sample* data, sample* coeffs, int len, int last_idx)
-{      
-    sample *dp = data;
-    sample *cp = coeffs;
-    longSample a[2] = {0.0, 0.0};
-   
-    dp = data+last_idx;
-    int cnt = len - last_idx;
-    for (; cnt >= 2; cnt -= 2)
-    {
-        a[0] += (longSample)*dp++ * (longSample)*cp++;
-        a[1] += (longSample)*dp++ * (longSample)*cp++;
-    }   
-
-    if (cnt)
-    {
-        a[0] += (longSample)*dp++ * (longSample)*cp++; 
-    }
-
-    
-    dp = data;
-    cnt = last_idx;
-    for (; cnt >= 2; cnt -= 2)
-    {
-        a[0] += (longSample)*dp++ * (longSample)*cp++;
-        a[1] += (longSample)*dp++ * (longSample)*cp++;
-    }
-
-    if (cnt)
-    {
-        a[1] += (longSample)*dp++ * (longSample)*cp++; 
-    }
-
-    return a[0] + a[1];
-}
-
-
-longSample conv_split_unrolled4_no_sep_result(sample* data, sample* coeffs, int len, int last_idx)
-{      
-    sample *dp = data;
-    sample *cp = coeffs;
-    longSample a = 0.0;
-   
-    dp = data+last_idx;
-    int cnt = len - last_idx;
-    for (; cnt >= 4; cnt -= 4)
-    {
-        a += (longSample)*dp++ * (longSample)*cp++;
-        a += (longSample)*dp++ * (longSample)*cp++;
-        a += (longSample)*dp++ * (longSample)*cp++;
-        a += (longSample)*dp++ * (longSample)*cp++;
-    }   
-
-    while (cnt--)
-    {
-        a += (longSample)*dp++ * (longSample)*cp++; 
-    }
-
-    
-    dp = data;
-    cnt = last_idx;
-    for (; cnt >= 4; cnt -= 4)
-    {
-        a += (longSample)*dp++ * (longSample)*cp++;
-        a += (longSample)*dp++ * (longSample)*cp++;
-        a += (longSample)*dp++ * (longSample)*cp++;
-        a += (longSample)*dp++ * (longSample)*cp++;
-    }
-
-    while (cnt--)
-    {
-        a += (longSample)*dp++ * (longSample)*cp++; 
-    }
-
-    return a;
-}
-
-longSample conv_split(sample* data, sample* coeffs, int len, int last_idx)
-{      
-    sample *dp = data;
-    sample *cp = coeffs;
-    longSample a = 0.0;
-   
-    dp = data+last_idx;
-    int cnt = len - last_idx;
-    for (; cnt >= 1; cnt -= 1)
-    {
-        a += (longSample)*dp++ * (longSample)*cp++;
-    }   
- 
-    dp = data;
-    cnt = last_idx;
-    for (; cnt >= 1; cnt -= 1)
-    {
-        a += (longSample)*dp++ * (longSample)*cp++;   
-    }
-
-    return a;
-}
-
-longSample conv_dummy_FPU(sample* data, sample* coeffs, int len, int last_idx)
-{
-    longSample a = 0;
-    sample* dp = data;
-    sample* cp = coeffs;
-
-    int data_idx = last_idx;
-
-    for (int i = 0; i < len; ++i)
-    {
-        a += (longSample)dp[data_idx] * (longSample)cp[i];
-		if (data_idx >= len)
-			data_idx = 0;
-    }
-
-    return a;
-}
-
-longSample conv_dummy(sample* data, sample* coeffs, int len, int last_idx)
-{
-	longSample a = 0;
-    sample* dp = data;
-    sample* cp = coeffs;
-
-    int data_idx = last_idx;
-
-    for (int i = 0; i < len; ++i)
-    {
-        a += dp[data_idx] * cp[i];
-		if (data_idx >= len)
-			data_idx = 0;
-    }
-
-    return a;
-}
-
 struct TestData
 {
     TestData(size_t noOfSamples): data(noOfSamples, 0), coeffs(noOfSamples, 0.0) 
@@ -239,22 +23,6 @@ struct TestData
     std::vector<sample> coeffs;
 };
 
-struct implementation
-{
-	const std::string name;
-	const convFun fun;
-};
-
-std::vector<implementation> implementations =
-{
-	{"Simple", 						conv_dummy},
-	{"SplitUnrolled4SepResults",	conv_split_unrolled4_sep_result},
-	{"SplitUnrolled2SepResults",	conv_split_unrolled2_sep_result},
-	{"SplitUnrolled4NoSepResults",  conv_split_unrolled4_no_sep_result},
-	{"SimpleForceFPU",	 			conv_dummy_FPU},
-	{"SimpleSplit", 				conv_split},
-	{"SplitUnrolled4SepResultsNoLong", conv_split_unrolled4_sep_result_no_long}
-};
 
 const int iterations = 10000;
 const int coeffs = 499;
@@ -279,7 +47,7 @@ int main()
 		printInfo();
     	TestData t(coeffs);
 
-    	for (auto& impl: implementations)
+    	for (auto& impl: Implementations::getInstance().getImplementations())
     	{
     		timediff td;
 
@@ -287,12 +55,12 @@ int main()
 
 	    	for (int j = 0; j < iterations; ++j)
 		    	for (int i = 0; i < t.data.size(); ++i)
-					result += impl.fun(t.data.data(), t.coeffs.data(), t.data.size(), i);
+					result += impl.function(t.data.data(), t.coeffs.data(), t.data.size(), i);
 			
 			double t = td.get_diff();
-			double MACs = iterations * (double)coeffs * (double)coeffs;
-
-			printf("%s:\n\tTime: % 3.4f\tMMACs/s: % 9.3f\tResult: % 30.15Lf\n", impl.name.c_str(), t, MACs / t / 1e6, result);
+		
+			printf("%s:\tUnrolled: %d SR: %d ForcedLong: %d\n", impl.name.c_str(), impl.unrolled, impl.separated, impl.forcedLong);
+			printf("\tTime: % 3.4f\tMMACs/s: % 9.3f\tResult: % 30.15Lf\n", t, MACs / t / 1e6, result);
     	}
 }
     	
--- types.h
+++ types.h
@@ -1,6 +1,47 @@
+#ifndef TYPES_H
+#define TYPES_H
+
 #include <functional>
+#include <vector>
+#include <string>
 
 using sample = double;
 using longSample = long double;
 
 using convFun = std::function<longSample (sample*, sample*, int, int)>;
+
+class Implementation
+{
+	public:
+		Implementation(const std::string& name, convFun function, int unrolled=1, bool separated=false, bool forcedLong = false):
+		name(name), function(function), unrolled(unrolled), separated(separated), forcedLong(forcedLong) {}
+		std::string		name;
+		convFun 		function;
+		int 			unrolled;
+		bool			separated;
+		bool			forcedLong; 
+};
+
+class Implementations
+{
+	public:
+		static Implementations& getInstance() {static Implementations impls; return impls;}
+		void addImplementation(const Implementation& i) {implementations.push_back(i);}
+		const std::vector<Implementation>& getImplementations() {return implementations;}
+ 
+	private:
+		std::vector<Implementation> implementations;
+};
+
+
+class RegisterImplementation
+{
+	public:
+		RegisterImplementation(const std::string& name, convFun function, int unrolled=1, bool separated=false, bool forcedLong = false)
+		{
+			Implementations::getInstance().addImplementation(Implementation(name, function, unrolled, separated, forcedLong));
+		}
+};
+
+
+#endif //TYPES_H
